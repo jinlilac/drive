9ad8223ac77e5355a40cb402bbe4e391c1e1d331
jinlilac24 <jinlilac24@gmail.com>
2025-03-21 17:43:07 +0900

    🎉 [Init]: design-system 설정
    - color styles
    - typography
    - global-styles css 초기화
    - styled component theme 파일 설정

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,47 @@
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+import 'pretendard/dist/web/static/pretendard.css';
+
+const GlobalStyle = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none; 
+}
+body{
+  font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+  -webkit-text-size-adjust:none;
+  overflow: hidden;
+  background-color: white;
+}
+button {
+  font:inherit
+}
+input[type="text"],
+input[type="password"],
+input[type="submit"],
+input[type="search"],
+input[type="tel"],
+input[type="email"],
+html input[type="button"],
+input[type="reset"] {
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  /* -webkit-border-radius:0;
+  -moz-border-radius:0; */
+  /* border-radius: 0; */
+}
+a {
+  text-decoration: none;
+}
+  div::-webkit-scrollbar{
+  display: none;
+  width: 0;  /* Remove scrollbar space */
+  height: 0;
+  background: transparent;  /* Optional: just make scrollbar invisible */
+  -webkit-appearance: none;
+  }
+`;
+export default GlobalStyle;
---
+++ src/styles/theme.ts
@@ -0,0 +1,65 @@
+import { DefaultTheme } from 'styled-components';
+
+const Colors = {
+  // system
+  error: '#DA1414',
+  fill: '#1B1B1B',
+  success: '#287D3C',
+  error_bg: '#FEEFEF',
+  success_bg: '#EDF9F0',
+  // grayscale
+  gray_10: '#FFFFFF',
+  gray_20: '#FAFAFA',
+  gray_30: '#F3F2F2',
+  gray_40: '#E9E8E7',
+  gray_50: '#D7D7D5',
+  gray_60: '#B5B5B5',
+  gray_70: '#8A8A8A',
+  gray_80: '#6B6B6B',
+  gray_90: '#4A4A4A',
+  gray_100: '#2F2F2F',
+  gray_200: '#1B1B1B',
+
+  // keycolor
+  red: '#FF7262',
+  blue: '#1ABCFE',
+  purple: '#A259FF',
+  green: '#0ACF83',
+  dark: '#10130C',
+
+  default: 'inherit',
+};
+
+const Font = {
+  fontWeight: {
+    bold: '700',
+    semiBold: '600',
+    medium: '500',
+    regular: '400',
+  },
+  fontSize: {
+    h1: '2.25rem',
+    h2: '2rem',
+    h3: '1.75rem',
+    t1: '1.5rem',
+    t2: '1.25rem',
+    t3: '1.125rem',
+    b1: '1rem',
+    b2: '0.875rem',
+    b3: '0.75rem',
+  },
+};
+const BorderRadius = {
+  '4': '0.25rem',
+  '8': '0.5rem',
+  '12': '0.75rem',
+  '16': '1rem',
+};
+
+const theme = {
+  Colors,
+  Font,
+  BorderRadius,
+} as DefaultTheme;
+
+export default theme;
