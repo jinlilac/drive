1e8c181edd7c0c1979e68bfbaaee752e1bd5575e
jinlilac24 <jinlilac24@gmail.com>
2025-03-28 19:19:14 +0900

    🚀 [Feat]: 환경 변수 타입 생성

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_KAKAO_CLIENT_ID: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_GOOGLE_REDIRECT_URI: string;
+  readonly VITE_NAVER_CLIENT_ID: string;
+  readonly VITE_NAVER_REDIRECT_URI: string;
+  readonly VITE_NAVER_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
