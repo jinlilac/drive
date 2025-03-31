0bf5fbd3bd53c22fb89efeef23587d85b8dea299
kwondns <kwondns@gmail.com>
2025-03-31 10:59:21 +0900

    🚀 [Feat]: SignUp Zod 설정
    - Zod 생성
    - name
    - password
    - confirmPassword
    - serviceAgreement
    - userAgreement
    
    - 회원가입 시에 사용되지 않는 code
    - 이메일 인증 시 사용

---
+++ src/types/signup.type.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+export const RequestEmailVerificationPayload = z.object({
+  email: z.string().email('잘못된 형식의 이메일 주소입니다.'),
+});
+
+export type RequestEmailVerificationType = z.infer<typeof RequestEmailVerificationPayload>;
+
+export const VerificationEmailPayload = RequestEmailVerificationPayload.extend({
+  code: z.number().min(100000).max(999999),
+});
+
+export type VerificationEmailType = z.infer<typeof VerificationEmailPayload>;
+
+export const PasswordValidate = z
+  .string({ required_error: '비밀번호를 입력해주세요.' })
+  .min(8, {
+    message: '영문, 숫자, 특수기호를 포함하여 8자리 이상 입력해주세요.',
+  })
+  .regex(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/, {
+    message: '영문, 숫자, 특수기호를 포함하여 8자리 이상 입력해주세요.',
+  });
+
+export const SignUpPayload = RequestEmailVerificationPayload.extend({
+  name: z.string().min(2, { message: '2자리 이상 입력해주세요.' }),
+  password: PasswordValidate,
+  confirmPassword: PasswordValidate,
+  serviceAgreement: z.literal<boolean>(true),
+  userAgreement: z.literal<boolean>(true),
+}).refine(
+  (data) => {
+    console.log(data);
+    return data.password === data.confirmPassword;
+  },
+  {
+    message: '비밀번호가 일치하지 않습니다.',
+    path: ['confirmPassword'],
+  },
+);
+
+export type SignUpType = z.infer<typeof SignUpPayload>;
