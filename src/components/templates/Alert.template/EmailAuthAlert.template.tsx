52f26b4fa8646fa575ca4692c62d74990b6451b8
kwondns <kwondns@gmail.com>
2025-03-31 15:43:54 +0900

    ♻️  [Refactor]: 이메일 인증 Alert 분리
    - 내용이 길어저 Alert 별도 분리

---
+++ src/components/templates/Alert.template/EmailAuthAlert.template.tsx
@@ -0,0 +1,25 @@
+import Container from '@/components/atoms/Container';
+import Img from '@/components/atoms/Img';
+import { ICON } from '@/constants/icon';
+import Typography from '@/components/atoms/Typography';
+
+function EmailAuthAlertTemplate() {
+  return (
+    <Container.FlexCol gap="12" alignItems="center" style={{ marginBottom: '12px' }}>
+      <Img style={{ width: '53px' }} src={ICON.confirm} />
+      <Typography.T2 fontWeight="bold" color="gray_100">
+        이메일 인증 번호를 발송하였습니다.
+      </Typography.T2>
+      <Container.FlexCol gap="4" alignItems="center">
+        <Typography.B1 fontWeight="medium" color="gray_70">
+          입력하신 메일로 수신하신 번호를 확인 후
+        </Typography.B1>
+        <Typography.B1 fontWeight="medium" color="gray_70">
+          인증 번호를 입력하여 인증을 완료하세요.
+        </Typography.B1>
+      </Container.FlexCol>
+    </Container.FlexCol>
+  );
+}
+
+export default EmailAuthAlertTemplate;
