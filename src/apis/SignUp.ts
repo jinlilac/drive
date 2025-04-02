e796e2c44ebff7bdaf09c18173febf1ef467c6e9
kwondns <kwondns@gmail.com>
2025-04-02 14:05:35 +0900

    🚀 [Feat]: 회원가입 완료 후 이동 추가
    - sign/profile로 이동 되도록 추가
    - Store에 회원가입 반환 데이터 저장 필요

--- src/apis/SignUp.ts
+++ src/apis/SignUp.ts
@@ -1,11 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { axiosInstance } from '@/libs/axios';
 import { SignUpType } from '@/types/signup.type';
+import { useNavigate } from 'react-router-dom';
 
 export const useSignUp = () => {
+  const navigate = useNavigate();
   const { mutate: signUp, isPending: isSigning } = useMutation({
     mutationFn: async (payload: Omit<SignUpType, 'confirmPassword'>) => {
       await axiosInstance.post('/user/sign-up', payload);
+    },
+    onSuccess: () => {
+      navigate('/sign/profile');
     },
   });
   return { signUp, isSigning };
