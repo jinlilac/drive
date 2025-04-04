6bc916e68cb343ecd380a598121bbaa6a7d4eb76
jinlilac24 <jinlilac24@gmail.com>
2025-04-04 17:29:09 +0900

    🚀 [Feat]: 원본파일 formdata 제출시 사용될 axios 생성

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -15,3 +15,18 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error.message || '서버 에러가 발생하였습니다.');
   },
 );
+export const axiosFormDataInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: { 'Content-Type': 'multipart/form-data' },
+  withCredentials: true,
+});
+
+axiosFormDataInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.data?.message) {
+      return Promise.reject(error.response.data.message);
+    }
+    return Promise.reject(error.message || '서버 에러가 발생하였습니다.');
+  },
+);
