74f614eda69af11021c57b387827168fe8eae480
jinlilac24 <jinlilac24@gmail.com>
2025-04-08 15:41:41 +0900

    🚀 [Feat]: 소셜 로그인 callbacktemplate 생성
    - 로딩적용
    - useOAuth hook 적용

--- src/components/templates/OAuth.tsx
+++ src/components/templates/OAuth.tsx
@@ -1,14 +1,50 @@
 import { useLocation, useSearchParams } from 'react-router-dom';
 import { useOAuth } from '@/apis/OAuth';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Loading from '@/components/atoms/Loading';
+import SignInTemplate from '@/components/templates/SignIn.template/SignIn.template';
+import Container from '@/components/atoms/Container';
+import useOverlayStore from '@/stores/useOverlayStore';
+import Overlay from '@/components/atoms/ Overlay';
+import { styled } from 'styled-components';
+
+const OauthWrap = styled(Container.FlexCol)`
+  position: relative;
+  height: 100%;
+  min-width: 480px;
+  justify-content: center;
+`;
+const LoaderWrap = styled(Container.FlexCol)`
+  position: absolute;
+  border-radius: 8px;
+  z-index: 999;
+  width: 100%;
+  align-items: center;
+`;
 
 export default function OAuth() {
   const [searchParams] = useSearchParams();
   const { pathname } = useLocation();
-  const social = pathname.split('/')[2];
-  const { oauth } = useOAuth(social);
+  const { openOverlay } = useOverlayStore();
+  const [loader, setLoader] = useState<boolean>(false);
+
+  const social = pathname.split('/')[3];
+  const { oauth, isPending } = useOAuth(social);
   useEffect(() => {
+    openOverlay();
+    setLoader(true);
     oauth({ code: searchParams.get('code') as string });
   }, []);
-  return <div>hello</div>;
+  return (
+    <OauthWrap>
+      <Overlay />
+      <Loading />
+      <SignInTemplate />
+      {loader && (
+        <LoaderWrap>
+          <Loading />
+        </LoaderWrap>
+      )}
+    </OauthWrap>
+  );
 }
