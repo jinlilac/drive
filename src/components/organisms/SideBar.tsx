7c876773cc0ce7a900c05254ca165052ae2813c0
jinlilac24 <jinlilac24@gmail.com>
2025-04-11 17:57:38 +0900

    🚀 [Feat]: SideBar 컴포넌트 생성
    - SideBarHeader 영역과 Body영역으로 구성됨

---
+++ src/components/organisms/SideBar.tsx
@@ -0,0 +1,27 @@
+import Container from '@/components/atoms/Container';
+import { ReactNode } from 'react';
+import styled, { CSSProperties } from 'styled-components';
+
+type SideBarProps = {
+  header?: ReactNode;
+  children?: ReactNode;
+  style?: CSSProperties;
+};
+
+const SideBarWrap = styled(Container.FlexCol)`
+  min-width: 300px;
+  padding: 0 16px;
+  border-right: 1px solid ${(props) => props.theme.Colors.gray_30};
+`;
+const SideBarHeader = styled.div`
+  padding-top: 24px;
+`;
+
+export default function SideBar({ header, children, style, ...others }: SideBarProps) {
+  return (
+    <SideBarWrap style={style}>
+      <SideBarHeader>{header}</SideBarHeader>
+      {children}
+    </SideBarWrap>
+  );
+}
