1f7b61c243a4a1e958e5cde692baf08e9c052c49
jinlilac24 <jinlilac24@gmail.com>
2025-04-17 17:37:30 +0900

    🚀 [Feat]: 작업 지시서 목록, 이름 바꾸기 hook 생성

---
+++ src/apis/WorkSheet.ts
@@ -0,0 +1,51 @@
+import { axiosInstance } from '@/libs/axios';
+import { useAuthStore } from '@/stores/useAuthStore';
+import { WorkSheetUpdatePayload } from '@/types/file.type';
+import { WorkSheetListType, WorkSheetResponseType } from '@/types/worksheet.type';
+import { infiniteQueryOptions, useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export const useGetWorkSheet = (filters: WorkSheetListType) => {
+  const isSignIn = !!localStorage.getItem('auth-store');
+  const { user } = useAuthStore();
+
+  return infiniteQueryOptions({
+    queryKey: ['worksheet', 'list', filters],
+    queryFn: async ({ pageParam }) => {
+      try {
+        const filterParams = Object.fromEntries(
+          Object.entries(filters).filter(([_, v]) => v !== undefined && v !== null),
+        );
+        // page는 항상 pageParam으로 덮어쓰기
+        const params = { ...filterParams, page: pageParam };
+        return await axiosInstance.get<WorkSheetResponseType>('/drive/worksheet', {
+          params,
+          headers: {
+            Authorization: `Bearer ${user?.accessToken}`,
+          },
+        });
+      } catch (e) {
+        if (e instanceof AxiosError && e.response?.status === 400) return { data: { count: -1 } };
+        throw e;
+      }
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, _allPages, lastPageParam, _allPageParams) =>
+      lastPage.data.count - lastPageParam * 30 > 0 ? lastPageParam + 1 : undefined,
+    enabled: isSignIn,
+  });
+};
+
+export const usePatchWorkSheet = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: patchWorkSheet, isPending } = useMutation({
+    mutationFn: async (payload: WorkSheetUpdatePayload) => axiosInstance.patch('/drive', payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['worksheet', 'list'] }).then(() => navigate('/workspace/work-sheet'));
+    },
+  });
+  return { patchWorkSheet, isPending };
+};
