1e68222af24b5d4af8cd4ed33a3fcf67e76c145d
jinlilac24 <jinlilac24@gmail.com>
2025-04-22 14:41:24 +0900

    🚀 [Feat]: ActionToolBar 컴포넌트 생성
    - 체크된 수, 즐겨찾기, 폴더, 삭제 등등..의 actions가 포함된 바
    - indeterminate, checked로 전체해제, 전체 선택 기능 구현

--- src/components/atoms/CheckBox.tsx
+++ src/components/atoms/CheckBox.tsx
@@ -3,8 +3,9 @@ import { ComponentProps } from 'react';
 import styled from 'styled-components';
 
 type CheckBoxProps = {
-  option: 'default' | 'square' | 'circle';
+  option: 'default' | 'square' | 'circle' | 'minus';
   checked?: boolean;
+  indeterminate?: boolean;
 } & Omit<ComponentProps<'input'>, 'type'>;
 
 const CheckboxContainer = styled.label`
@@ -28,27 +29,51 @@ const CustomCheckBox = styled.div<CheckBoxProps>`
   padding: 2px;
   background-color: white;
 
-  ${(props) => {
-    switch (props.option) {
+  ${({ option, checked, indeterminate, theme }) => {
+    const getBackground = () => {
+      if (indeterminate) return `url('${ICON['minus-box']}')`;
+      if (checked) return `url('${ICON['square-white']}')`;
+      return 'none';
+    };
+
+    switch (option) {
       case 'default':
         return `
-          border: 2px solid ${props.theme.Colors.gray_50};
+          border: 2px solid ${theme.Colors.gray_50};
           border-radius: 4px;
           background-color: white;
+          ${
+            checked
+              ? `background-image: url('${ICON['square-check']}');
+         background-repeat: no-repeat;
+         background-position: center;
+         border:none`
+              : ''
+          }
         `;
       case 'square':
         return `
           border: none;
-          background-image: url('${ICON[props.checked ? 'square-check' : 'square-check-off']}');
+          background-image: url('${ICON[checked ? 'square-check' : 'square-check-off']}');
           background-repeat: no-repeat;
           background-position: center;
         `;
       case 'circle':
         return `
           border: none;
-          background-image: url('${ICON[props.checked ? 'circle-check' : 'circle-check-off']}');
+          background-image: url('${ICON[checked ? 'circle-check' : 'circle-check-off']}');
           background-repeat: no-repeat;
           background-position: center;
+        `;
+      case 'minus':
+        return `
+          width: 15px;
+          height: 15px;
+          border: ${!checked && !indeterminate ? `1px solid ${theme.Colors.gray_40}` : 'none'};
+          border-radius: 2px;
+          background: ${getBackground()} center/contain no-repeat;
+          background-position: center;
+          background-color: ${(checked || indeterminate) ?? 'transparent'};
         `;
       default:
         return '';
@@ -59,8 +84,8 @@ const CustomCheckBox = styled.div<CheckBoxProps>`
 const CheckBox = ({ option, checked, onChange, ...props }: CheckBoxProps) => {
   return (
     <CheckboxContainer>
-      <HiddenCheckBox checked={checked} onChange={onChange} {...props} />
-      <CustomCheckBox option={option} checked={checked} />
+      <HiddenCheckBox checked={checked} onChange={onChange} />
+      <CustomCheckBox option={option} checked={checked} {...props} />
     </CheckboxContainer>
   );
 };
---
+++ src/components/organisms/ActionToolBar.tsx
@@ -0,0 +1,108 @@
+// components/organisms/ActionToolbar.tsx
+import Button from '@/components/atoms/Button';
+import CheckBox from '@/components/atoms/CheckBox';
+import Container from '@/components/atoms/Container';
+import Divider from '@/components/atoms/Divider';
+import Img from '@/components/atoms/Img';
+import Typography from '@/components/atoms/Typography';
+import { ICON } from '@/constants/icon';
+import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+export type Action = {
+  type: 'star' | 'unstar' | 'folder' | 'delete' | 'restore' | 'permanent-delete' | 'custom'; // 확장 가능한 액션 타입
+  label: string;
+  icon?: keyof typeof ICON;
+  handler: (ids: string[]) => void;
+};
+
+export type Props = {
+  selectedIds: string[];
+  allIds: string[];
+  setSelectedIds: Dispatch<SetStateAction<string[]>>;
+  actions: Action[];
+  close: () => void;
+};
+
+type IconButtonProps = {
+  src: keyof typeof ICON;
+  label: string;
+  onClick: () => void;
+};
+
+const Toolbar = styled(Container.FlexRow)`
+  position: absolute;
+  width: 100%;
+  max-width: 622px;
+  top: 4px;
+  left: 157px;
+  background: ${(props) => props.theme.Colors.gray_100};
+  border-radius: 8px;
+  padding: 8px 16px;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
+  z-index: 100;
+`;
+
+const SelectionInfo = styled(Typography.B2)`
+  font-weight: ${(props) => props.theme.Font.fontWeight.semiBold};
+  color: white;
+`;
+const ButtonIcon = styled(Button.Ghost)`
+  display: flex;
+  gap: 4px;
+  padding: 4px 8px;
+  align-items: center;
+`;
+const IconButton = ({ src, label, onClick }: IconButtonProps) => {
+  return (
+    <ButtonIcon onClick={onClick}>
+      <Img src={ICON[src]} />
+      <Typography.B2 fontWeight="semiBold" color="gray_10">
+        {label}
+      </Typography.B2>
+    </ButtonIcon>
+  );
+};
+
+export default function ActionToolbar({ selectedIds, allIds, setSelectedIds, actions, close }: Props) {
+  const isAllSelected = selectedIds.length === allIds.length && allIds.length > 0;
+  const isIndeterminate = selectedIds.length > 0 && selectedIds.length < allIds.length;
+
+  // 체크박스 클릭 시 동작
+  const handleSelectAll = () => {
+    if (isAllSelected || isIndeterminate) {
+      setSelectedIds([]); // 모두 해제
+    } else {
+      setSelectedIds(allIds); // 모두 선택
+    }
+  };
+
+  return (
+    <Toolbar>
+      <Container.FlexRow gap="42">
+        <Container.FlexRow gap="4" alignItems="center" style={{ minWidth: '73px' }}>
+          <CheckBox option="minus" checked={isAllSelected} indeterminate={isIndeterminate} onChange={handleSelectAll} />
+          <SelectionInfo>{selectedIds.length}개 선택</SelectionInfo>
+        </Container.FlexRow>
+
+        <Container.FlexRow gap="8" justifyContent="center">
+          {actions.map((action) => (
+            <Container.FlexRow gap="8" key={action.type} justifyContent="center" alignItems="center">
+              <IconButton
+                onClick={() => action.handler(selectedIds)}
+                src={action.icon as keyof typeof ICON}
+                label={action.label}
+              />
+              <Divider.Col color="gray_10" />
+            </Container.FlexRow>
+          ))}
+        </Container.FlexRow>
+      </Container.FlexRow>
+      <ButtonIcon onClick={close}>
+        <Img src={ICON['all-clear']} />
+      </ButtonIcon>
+    </Toolbar>
+  );
+}
