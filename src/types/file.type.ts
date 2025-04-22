c3c4e4c627dada6d47327aa8ece845a839eb52ca
jinlilac24 <jinlilac24@gmail.com>
2025-04-22 14:48:04 +0900

    ♻️ [Refactor]: WorkSheetUpdatePayload Type 리팩토링
    - id 제외하고 optional로 설정

--- src/types/file.type.ts
+++ src/types/file.type.ts
@@ -1,6 +1,24 @@
 export type WorkSheetUpdatePayload = {
   id: string[];
-  name: string[];
-  parentId: string[];
-  isStarred: boolean[];
+  name?: string[];
+  parentId?: string[];
+  isStarred?: boolean[];
+};
+
+export type FolderListResponse = {
+  children: string[];
+  createdAt: string;
+  deletedAt: null | string;
+  discriminator: string;
+  id: string;
+  isStarred: boolean;
+  mimetype: string; // 확장자
+  name: string;
+  path: string;
+  size: number;
+  starredAt: null | string;
+  storagePath: string;
+  tag: string;
+  type: string;
+  updatedAt: null | string;
 };
