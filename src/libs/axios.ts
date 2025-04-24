943e4d1f5ce837a551d877b5a8349dd3bd49376b
kwondns <kwondns@gmail.com>
2025-04-24 16:29:40 +0900

    🚑️ [Fix]: axios Refresh 수정
    - 로직 수정

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -7,15 +7,6 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.data?.message) {
-      return Promise.reject(error.response.data.message);
-    }
-    return Promise.reject(error.message || '서버 에러가 발생하였습니다.');
-  },
-);
 export const axiosFormDataInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'multipart/form-data' },
@@ -49,24 +40,29 @@ const axiosResponseInterceptor = axiosInstance.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
     const userLocal = JSON.parse(localStorage.getItem('auth-store') ?? '{}');
-    if (!userLocal.userId) {
+    if (!userLocal.state.user.userId) {
       return Promise.reject(error);
     }
+    const user = userLocal.state.user;
     const status = error.response?.status;
     if (status === 401 || status === 400) {
       const originalRequest = error.config;
       const refreshTokenFn = async () => {
         try {
-          if (userLocal.social !== null && userLocal.social !== undefined) {
-            const { data } = await axiosInstance.post('/user/refresh', {
-              headers: { 'Content-Type': 'application/json' },
-              withCredentials: true,
-            });
+          if (user.social !== null && user.social !== undefined) {
+            const { data } = await axios.post(
+              `${import.meta.env.VITE_API_URL}/user/refresh`,
+              {},
+              {
+                headers: { 'Content-Type': 'application/json' },
+                withCredentials: true,
+              },
+            );
             // 새로운 토큰 저장
-            const { accessToken: newAccessToken, expiresAt: newExpiresAt } = data;
-            userLocal.accessToken = newAccessToken;
-            userLocal.expiresAt = newExpiresAt;
-            localStorage.setItem('auth-store', JSON.stringify(userLocal));
+            const { accessToken: newAccessToken, accessTokenExpiresAt: newExpiresAt } = data;
+            user.accessToken = newAccessToken;
+            user.accessTokenExpiresAt = newExpiresAt;
+            localStorage.setItem('auth-store', JSON.stringify(user));
           }
         } catch (error) {
           const refreshTokenError = error as AxiosError;
@@ -78,10 +74,10 @@ const axiosResponseInterceptor = axiosInstance.interceptors.response.use(
       try {
         await RefreshManager.execute(refreshTokenFn); // RefreshManager 사용
         if (originalRequest) {
-          const newAccessToken = userLocal.accessToken; // 업데이트된 토큰 사용
+          const newAccessToken = user.accessToken; // 업데이트된 토큰 사용
           if (newAccessToken) {
             originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-            originalRequest.headers.social = userLocal.social;
+            originalRequest.headers.social = user.social;
             return axios(originalRequest); // 원래 요청을 재시도
           }
         }
@@ -94,7 +90,18 @@ const axiosResponseInterceptor = axiosInstance.interceptors.response.use(
     return null;
   },
 );
-
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.data?.message) {
+      return Promise.reject({ message: error.response.data.message, status: error.response.status });
+    }
+    return Promise.reject({
+      message: error.response.data.message || '서버 에러가 발생하였습니다.',
+      status: error.response.status,
+    });
+  },
+);
 export const removeDefaultInterceptors = () => {
   if (axiosRequestInterceptor !== undefined) {
     axiosInstance.interceptors.request.eject(axiosRequestInterceptor);
