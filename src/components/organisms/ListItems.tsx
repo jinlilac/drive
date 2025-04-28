14fa033a0d30627bac83e8429cfefa12dbc8ade1
kwondns <kwondns@gmail.com>
2025-04-28 16:02:13 +0900

    ♻️ [Refactor]: ListItems 수정
    - Drive 페이지는 별도의 List 형태를 가져 분별할 수 있도록 변경

--- src/components/organisms/ListItems.tsx
+++ src/components/organisms/ListItems.tsx
@@ -1,15 +1,27 @@
 import FileList from '@/components/organisms/FileList';
 import { UpdateState } from '@/components/templates/WorkSpace.template/WorkSheetBaseTemplate';
 import { WorkSheetItems } from '@/types/worksheet.type';
+import { FileSystemType } from '@/types/workspace.type';
 
 export const ListItems = ({
   content,
   checked,
   setState,
   onCheck,
+  isDrive = false,
 }: {
-  content: WorkSheetItems;
+  content: WorkSheetItems | FileSystemType;
   checked: boolean;
   setState: React.Dispatch<React.SetStateAction<UpdateState>>;
   onCheck: (id: string, checked: boolean) => void;
-}) => <FileList key={content.fileSystemId} {...content} setState={setState} checked={checked} onCheck={onCheck} />;
+  isDrive?: boolean;
+}) => (
+  <FileList
+    key={content.fileSystemId}
+    {...content}
+    setState={setState}
+    checked={checked}
+    onCheck={onCheck}
+    isDrive={isDrive}
+  />
+);
