5703c10f84c1d339f521ece001e9e9b92a48f159
kwondns <kwondns@gmail.com>
2025-04-29 18:46:47 +0900

    ♻️ [Refactor]: CardItems 수정
    - isFolder의 값에 따라 FolderCard를 보여질 수 있도록 수정

--- src/components/organisms/CardItems.tsx
+++ src/components/organisms/CardItems.tsx
@@ -1,15 +1,31 @@
 import FileCard from '@/components/organisms/FileCard';
 import { UpdateState } from '@/components/templates/WorkSpace.template/WorkSheetBaseTemplate';
 import { WorkSheetItems } from '@/types/worksheet.type';
+import { FileSystemType } from '@/types/workspace.type';
+import FolderCard from '@/components/organisms/FolderCard';
 
 export const CardItems = ({
   content,
   checked,
   setState,
   onCheck,
+  isFolder = false,
 }: {
-  content: WorkSheetItems;
+  content: WorkSheetItems | FileSystemType;
   checked: boolean;
   setState: React.Dispatch<React.SetStateAction<UpdateState>>;
   onCheck: (id: string, checked: boolean) => void;
-}) => <FileCard key={content.fileSystemId} {...content} setState={setState} checked={checked} onCheck={onCheck} />;
+  isFolder?: boolean;
+}) => {
+  return isFolder ? (
+    <FolderCard
+      key={content.fileSystemId}
+      {...(content as FileSystemType)}
+      setState={setState}
+      checked={checked}
+      onCheck={onCheck}
+    />
+  ) : (
+    <FileCard key={content.fileSystemId} {...content} setState={setState} checked={checked} onCheck={onCheck} />
+  );
+};
