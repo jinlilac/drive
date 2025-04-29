23875d4161ed5ed33aefcc22fdc7bd5e59553121
kwondns <kwondns@gmail.com>
2025-04-29 18:45:03 +0900

    🚀 [Feat]: 즐겨찾기, 휴지통 페이지 생성

---
+++ src/components/pages/WorkSpace/Trash.tsx
@@ -0,0 +1,56 @@
+import Container from '@/components/atoms/Container';
+import styled from 'styled-components';
+import FilterBar from '@/components/organisms/FilterBar';
+import { useState } from 'react';
+import { WorkSheetListType } from '@/types/worksheet.type';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import { useSetSearchParam } from '@/hooks/useSearchParam';
+import { FileSystemAllResponseType, FileSystemListResponseType, KorToEngDriveCategory } from '@/types/workspace.type';
+import CategoryTab from '@/components/organisms/CategoryTab';
+import { AxiosResponse } from 'axios';
+import WorkSpaceStarAndTrashTemplate from '@/components/templates/WorkSpace.template/WorkSpaceStarAndTrash.template';
+import { useGetTrash } from '@/apis/Trash';
+
+const WorkSpaceContainer = styled(Container.FlexCol)`
+  width: 100%;
+  height: 100%;
+  padding: 0 8px;
+  overflow: scroll;
+  position: relative;
+`;
+
+export default function Trash() {
+  const { add, get } = useSetSearchParam();
+  const viewMode = get('view') === 'list' ? 'list' : 'card';
+  const [currentTab, setCurrentTab] = useState<KorToEngDriveCategory>(KorToEngDriveCategory.전체);
+  const [filters, _setFilters] = useState<WorkSheetListType>({
+    page: 1,
+    name: get('name') || undefined,
+  });
+  // 필터 선택 핸들러
+
+  const handleViewMode = (mode: 'card' | 'list') => {
+    add([['view', mode]]);
+  };
+
+  const { data, hasNextPage, fetchNextPage } = useSuspenseInfiniteQuery(useGetTrash(filters, currentTab));
+  return (
+    <WorkSpaceContainer>
+      <FilterBar
+        title="휴지통"
+        onClickList={() => handleViewMode('list')}
+        onClickCard={() => handleViewMode('card')}
+        isListActive={viewMode === 'list'}
+        isCardActive={viewMode === 'card'}
+      />
+      <CategoryTab currentTab={currentTab} setCurrentTab={setCurrentTab} />
+      <WorkSpaceStarAndTrashTemplate
+        fileSystem={data?.pages as AxiosResponse<FileSystemAllResponseType | FileSystemListResponseType>[]}
+        hasNextPage={hasNextPage}
+        fetchNextPage={fetchNextPage}
+        viewMode={viewMode}
+        currentTab={currentTab}
+      />
+    </WorkSpaceContainer>
+  );
+}
