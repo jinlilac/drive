949e53d3fc83396b75706eb878bdb59092323bcf
kwondns <kwondns@gmail.com>
2025-04-29 18:41:11 +0900

    🚀 [Feat]: 임시 FolderCard 생성
    - 일반 파일의 형태와 다른 Folder용 카드 생

---
+++ src/components/organisms/FolderCard.tsx
@@ -0,0 +1,123 @@
+import CheckBox from '@/components/atoms/CheckBox';
+import Container from '@/components/atoms/Container';
+import Img from '@/components/atoms/Img';
+import Typography from '@/components/atoms/Typography';
+import DropdownButton from '@/components/molecles/DropdownButton';
+import { DropBoxItem } from '@/components/molecles/ProfileCard';
+import { TagLabelProps } from '@/components/molecles/TagLabel';
+import { UpdateState } from '@/components/templates/WorkSpace.template/WorkSheetBaseTemplate';
+import { ICON } from '@/constants/icon';
+import { MORE_ITEMS } from '@/constants/worksheet';
+import useOverlayStore from '@/stores/useOverlayStore';
+import { FolderListResponse } from '@/types/file.type';
+import { Dispatch, SetStateAction } from 'react';
+import styled from 'styled-components';
+import { FileSystemType } from '@/types/workspace.type';
+
+const CardContainer = styled(Container.FlexRow)<{ checked: boolean }>`
+  width: 240px;
+  gap: 8px;
+  padding: 4px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  background-color: ${(props) => (props.checked ? props.theme.Colors.gray_30 : props.theme.Colors.gray_20)};
+  &:hover {
+    background-color: ${(props) => props.theme.Colors.gray_30};
+  }
+  position: relative;
+`;
+const CheckboxContainer = styled.div<{ checked: boolean }>`
+  position: absolute;
+  top: 8px;
+  left: 8px;
+  opacity: 0;
+  transition: opacity 0.2s;
+  opacity: ${(props) => (props.checked ? 1 : 0)};
+  ${CardContainer}:hover & {
+    opacity: 1;
+  }
+`;
+const TitleWrap = styled(Container.FlexRow)`
+  justify-content: space-between;
+  align-items: center;
+`;
+const Title = styled(Typography.B1)`
+  max-width: 152px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+const SubtitleWrap = styled(Container.FlexRow)``;
+
+const MoreItem = styled(DropBoxItem)`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`;
+
+export default function FolderCard(
+  props: FileSystemType &
+    TagLabelProps & {
+      setState: Dispatch<SetStateAction<UpdateState>>;
+      checked: boolean;
+      onCheck: (id: string, checked: boolean) => void;
+    } & Partial<FolderListResponse>,
+) {
+  const { name, setState, fileSystemId, childrenCount, parentId, checked, onCheck } = props;
+  const { openOverlay } = useOverlayStore();
+
+  const handleMoreButton = (action: string) => {
+    if (action === '이름 바꾸기') {
+      setState((prev) => ({
+        ...prev,
+        isOpen: true,
+        defaultName: name,
+        fileSystemId,
+        parentId,
+      }));
+
+      openOverlay();
+    } else if (action === '삭제') {
+      handleDelete();
+    }
+  };
+  const handleDelete = () => {
+    console.log('삭제');
+  };
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onCheck(fileSystemId, e.target.checked);
+  };
+  return (
+    <CardContainer id={fileSystemId} checked={checked}>
+      <CheckboxContainer checked={checked}>
+        <CheckBox option="default" checked={checked} onChange={handleCheckboxChange} />
+      </CheckboxContainer>
+      <Container.FlexRow style={{ width: '48px', height: '48px', padding: '12px' }}>
+        <Img fit="fill" src={ICON['folder-gray']} />
+      </Container.FlexRow>
+      <Container.FlexCol gap="8" style={{ flex: 1, padding: '4px' }}>
+        <TitleWrap>
+          <Title fontWeight="semiBold" color="gray_100">
+            {name}
+          </Title>
+        </TitleWrap>
+        <SubtitleWrap gap="4" alignItems="center">
+          <Typography.B3 fontWeight="medium" color="gray_70">
+            {childrenCount}개
+          </Typography.B3>
+        </SubtitleWrap>
+      </Container.FlexCol>
+      <DropdownButton isHover icon={<Img src={ICON.more} />}>
+        <Container.FlexCol>
+          {MORE_ITEMS.map((item) => (
+            <MoreItem key={item.label} onClick={() => handleMoreButton(item.label)}>
+              <Img src={item.icon} />
+              {item.label}
+            </MoreItem>
+          ))}
+        </Container.FlexCol>
+      </DropdownButton>
+    </CardContainer>
+  );
+}
