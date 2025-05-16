e1f8f2aa5d8f99b29651b7e4ac1a917adc84ae79
jinlilac24 <jinlilac24@gmail.com>
2025-05-16 13:47:29 +0900

    🚀 [Feat]: Drive page 컴포넌트 생성
    - BreadCrumb pathId로 클릭시 해당 폴더로 이동
    - filters 상태를 searchParams에서 가져온 path, category, page로 설정
    - currentTab category와 연결

---
+++ src/components/pages/Drive/Drive.tsx
@@ -0,0 +1,152 @@
+import { useGetDrive } from '@/apis/Drive';
+import Container from '@/components/atoms/Container';
+import Img from '@/components/atoms/Img';
+import Typography from '@/components/atoms/Typography';
+import CategoryTab from '@/components/organisms/CategoryTab';
+import FilterBar from '@/components/organisms/FilterBar';
+import { WorkSpaceContainer } from '@/components/pages/WorkSpace/Starred';
+import WorkSpaceTemplate from '@/components/templates/WorkSpace.template/WorkSpace.template';
+import { ICON } from '@/constants/icon';
+import { useSetSearchParam } from '@/hooks/useSearchParam';
+import { useAuthStore } from '@/stores/useAuthStore';
+import { FileSystemAllResponseType, FileSystemListResponseType, KorToEngDriveCategory } from '@/types/workspace.type';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+const BreadCrumb = styled(Container.FlexRow)`
+  width: 100%;
+  padding-top: 16px;
+  background: white;
+`;
+
+const BreadCrumbContainer = styled(Container.FlexRow)`
+  gap: 4px;
+
+  & > p {
+    max-width: 100px;
+    padding: 5px 0;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    align-self: center;
+  }
+
+  flex-wrap: wrap;
+`;
+const BreadCrumbLink = styled(Link)`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`;
+
+export default function Drive() {
+  const { add, get } = useSetSearchParam();
+  const viewMode = get('view') === 'list' ? 'list' : 'card';
+  const [title, setTitle] = useState(get('name'));
+
+  const [filters, setFilters] = useState(() => ({
+    category: get('category') as KorToEngDriveCategory,
+    path: get('path') as string,
+    page: Number(get('page')),
+  }));
+
+  useEffect(() => {
+    setTitle(get('name'));
+    setFilters({
+      category: get('category') as KorToEngDriveCategory,
+      path: get('path') as string,
+      page: Number(get('page')),
+    });
+  }, [get('category'), get('path'), get('page'), get('name')]);
+
+  const handleViewMode = (mode: 'card' | 'list') => {
+    add([['view', mode]]);
+  };
+  const { data, hasNextPage, fetchNextPage } = useSuspenseInfiniteQuery(useGetDrive(filters));
+  const responseData = data.pages[0].data;
+  const { setUser } = useAuthStore();
+
+  function isAllResponseType(
+    data: FileSystemAllResponseType | FileSystemListResponseType | { count: number },
+  ): data is FileSystemAllResponseType {
+    return 'idPath' in data && 'path' in data;
+  }
+  let fileSystemPath = '';
+  let fileSystemId = '';
+
+  if (isAllResponseType(responseData)) {
+    fileSystemPath = responseData.path;
+    fileSystemId = responseData.idPath;
+  }
+
+  const extractFileSystemPath = (path: string): string[] => {
+    return ['내 드라이브', ...path.split('/').slice(1)];
+  };
+  const extractFileSystemId = (idPath: string): string[] => {
+    return [...idPath.split('/').slice(1)];
+  };
+
+  const pathArr = extractFileSystemPath(fileSystemPath);
+  const idArr = extractFileSystemId(fileSystemId);
+
+  return (
+    <WorkSpaceContainer>
+      <Container.FlexCol gap="8">
+        {fileSystemPath && fileSystemId && (
+          <BreadCrumb>
+            <BreadCrumbContainer>
+              {pathArr.map((value, index, array) => {
+                const idPathForLink = idArr[index];
+
+                return (
+                  <BreadCrumbLink
+                    to={`/workspace/drive?page=1&path=${idPathForLink}&category=all&name=${encodeURIComponent(value)}&view=${viewMode}`}
+                    key={value}
+                    onClick={() =>
+                      setUser({
+                        currentId: idPathForLink,
+                      })
+                    }
+                  >
+                    <Typography.B2 fontWeight="medium" color="gray_90">
+                      {value}
+                    </Typography.B2>
+                    {index !== array.length - 1 && (
+                      <div style={{ width: '16px', height: '16px', alignSelf: 'center', textAlign: 'center' }}>
+                        <Img src={ICON['right-arrow']} />
+                      </div>
+                    )}
+                  </BreadCrumbLink>
+                );
+              })}
+            </BreadCrumbContainer>
+          </BreadCrumb>
+        )}
+
+        <FilterBar
+          title={title}
+          onClickCard={() => handleViewMode('card')}
+          onClickList={() => handleViewMode('list')}
+          isCardActive={viewMode === 'card'}
+          isListActive={viewMode === 'list'}
+        />
+      </Container.FlexCol>
+      <CategoryTab
+        currentTab={filters.category}
+        setCurrentTab={(category) => {
+          add([['category', category]]);
+        }}
+      />
+      <WorkSpaceTemplate
+        fileSystem={data.pages as AxiosResponse<FileSystemAllResponseType | FileSystemListResponseType>[]}
+        hasNextPage={hasNextPage}
+        fetchNextPage={fetchNextPage}
+        viewMode={viewMode}
+        currentTab={filters.category}
+      />
+    </WorkSpaceContainer>
+  );
+}
