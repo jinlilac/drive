e006c29677bc5d18bb0751385e184fc5f6eb39f8
kwondns <kwondns@gmail.com>
2025-05-20 16:20:33 +0900

    🚑️ [Fix]: queryKey  수정
    - useEffect로 페이지 이동 시 감지

--- src/hooks/useGetQueryKey.ts
+++ src/hooks/useGetQueryKey.ts
@@ -1,14 +1,19 @@
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export const useGetQueryKey = () => {
   const location = useLocation();
-  const pathname = location.pathname.split('/').slice(1);
-  const search = Object.fromEntries(
-    location.search
-      .slice(1)
-      .split('&')
-      .map((item) => item.split('='))
-      .filter((v) => v[0] !== 'name'),
-  );
-  return [...pathname, search];
+  const [queryKey, setQueryKey] = useState<string[]>([]);
+  useEffect(() => {
+    const pathname = location.pathname.split('/').slice(1);
+    const search = Object.fromEntries(
+      location.search
+        .slice(1)
+        .split('&')
+        .map((item) => item.split('='))
+        .filter((v) => v[0] !== 'name'),
+    );
+    setQueryKey([...pathname, search]);
+  }, [location]);
+  return queryKey;
 };
